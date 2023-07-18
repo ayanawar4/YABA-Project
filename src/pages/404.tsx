97aae261d7181cdc515cd3c06403bf610378c6a3
ayanawar4 <a.nawar@bimventures.com>
2023-07-18 13:35:50 +0200

    solving error of console

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
     <Layout>
       <div className="container mx-auto">
         <main className="mx-auto flex w-full flex-auto flex-col justify-center px-6 py-24 sm:py-32 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-primary">500</p>
+          <p className="text-base font-semibold leading-8 text-primary">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
           <p className="mt-6 text-base leading-7 text-white">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10">
